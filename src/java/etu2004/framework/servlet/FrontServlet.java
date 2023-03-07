eb708131387349009248ddd600162e0674f3e9bd
Andriniaina Fabien Rakotomanana <fabrakoto0987@gmail.com>
2023-03-07 09:19:27 +0300

    refactorisation

--- src/java/etu2004/framework/servlet/FrontServlet.java
+++ src/java/etu2004/framework/servlet/FrontServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlet;
+package etu2004.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
