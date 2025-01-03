d0fefe84869aa447cc27e09e226f89f6db470e05
Andriniaina Fabien Rakotomanana <fabrakoto0987@gmail.com>
2025-01-03 14:32:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-class_path="/home/fabien/Documents/GitHub/Framework/Framework/build/web/WEB-INF/classes"
-librairies_path="/home/fabien/Documents/GitHub/Framework/Test-framework/WEB-INF/lib"
-test_class_path="/home/fabien/Documents/GitHub/Framework/Test-framework/WEB-INF/classes"
+class_path="$HOME/GitHub/Framework/Framework/build/web/WEB-INF/classes"
+librairies_path="$HOME/Documents/GitHub/Framework/Test-framework/WEB-INF/lib"
+test_class_path="$HOME/Documents/GitHub/Framework/Test-framework/WEB-INF/classes"
 
 cd $class_path
 jar -cf etu2004Files.jar .
